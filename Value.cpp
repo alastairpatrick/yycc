8a2a422808a24f09296044700758904ed14faf9d
Al <alastairpatrick@gmail.com>
2023-04-25 07:54:48 -0700

    make most Value member functions inline

--- Value.cpp
+++ Value.cpp
@@ -1,34 +1,5 @@
 #include "Value.h"
 
-Value::Value(const Type* type, LLVMValueRef llvm)
-    : kind(llvm ? ValueKind::RVALUE : ValueKind::TYPE_ONLY), llvm(llvm), type(type), qualifiers(type->qualifiers()) {
-    assert(type);
-}
-
-Value::Value(ValueKind kind, const Type* type, LLVMValueRef llvm)
-    : kind(kind), llvm(llvm), type(type), qualifiers(type->qualifiers()) {
-    assert(type);
-    assert(kind == ValueKind::TYPE_ONLY || llvm);
-}
-
-bool Value::is_const() const {
-    return kind == ValueKind::RVALUE && LLVMIsConstant(llvm);
-}
-
-bool Value::is_const_integer() const {
-    return kind == ValueKind::RVALUE && LLVMIsAConstantInt(llvm);
-}
-
-LLVMValueRef Value::llvm_const_rvalue() const {
-    assert(is_const());
-    return llvm;
-}
-
-LLVMValueRef Value::llvm_lvalue() const {
-    assert(llvm && kind == ValueKind::LVALUE);
-    return llvm;
-}
-
 LLVMValueRef Value::llvm_rvalue(LLVMBuilderRef builder) const {
     assert(llvm);
     if (kind == ValueKind::RVALUE) {
@@ -40,20 +11,6 @@ LLVMValueRef Value::llvm_rvalue(LLVMBuilderRef builder) const {
         }
         return load;
     }
-}
-
-Value Value::unqualified() const {
-    return bit_cast(type->unqualified());
-}
-
-Value Value::bit_cast(const Type* type) const {
-    auto result(*this);
-    result.type = type;
-    return result;
-}
-
-Value Value::load(LLVMBuilderRef builder) const {
-    return Value(type, llvm_rvalue(builder));
 }
 
 void Value::store(LLVMBuilderRef builder, const Value& new_value) const {
--- Value.h
+++ Value.h
@@ -15,16 +15,56 @@ struct Value {
     unsigned qualifiers{};
 
     Value() = default;
-    Value(const Type* type, LLVMValueRef llvm = nullptr);
-    Value(ValueKind kind, const Type* type, LLVMValueRef llvm);
-    bool is_const() const;
-    bool is_const_integer() const;
-    LLVMValueRef llvm_const_rvalue() const;
-    LLVMValueRef llvm_lvalue() const;
+
+    Value(const Type* type, LLVMValueRef llvm = nullptr)
+            : kind(llvm ? ValueKind::RVALUE : ValueKind::TYPE_ONLY), llvm(llvm), type(type), qualifiers(type->qualifiers()) {
+        assert(type);
+    }
+
+    Value(ValueKind kind, const Type* type, LLVMValueRef llvm)
+        : kind(kind), llvm(llvm), type(type), qualifiers(type->qualifiers()) {
+        assert(type);
+        assert(kind == ValueKind::TYPE_ONLY || llvm);
+    }
+
+    bool is_valid() const {
+        return type;
+    }
+
+    bool is_const() const {
+        return kind == ValueKind::RVALUE && LLVMIsConstant(llvm);
+    }
+
+    bool is_const_integer() const{
+        return kind == ValueKind::RVALUE && LLVMIsAConstantInt(llvm);
+    }
+
+    LLVMValueRef llvm_const_rvalue() const{
+        assert(is_const());
+        return llvm;
+    }
+
+    LLVMValueRef llvm_lvalue() const{
+        assert(llvm && kind == ValueKind::LVALUE);
+        return llvm;
+    }
+
     LLVMValueRef llvm_rvalue(LLVMBuilderRef builder) const;
-    Value unqualified() const;
-    Value bit_cast(const Type* type) const;
-    Value load(LLVMBuilderRef builder) const;
+
+    Value unqualified() const {
+        return bit_cast(type->unqualified());
+    }
+
+    Value bit_cast(const Type* type) const {
+        auto result(*this);
+        result.type = type;
+        return result;
+    }
+
+    Value load(LLVMBuilderRef builder) const {
+        return Value(type, llvm_rvalue(builder));
+    }
+
     void store(LLVMBuilderRef builder, const Value& new_value) const;
 
 private:
