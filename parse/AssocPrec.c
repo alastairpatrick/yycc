ea24736a70e48cb7a466a137cbe59820de786d10
Al <alastairpatrick@gmail.com>
2023-04-25 20:43:13 -0700

    clean up

--- parse/AssocPrec.c
+++ parse/AssocPrec.c
@@ -8,12 +8,8 @@ const AssocPrec g_assoc_prec[TOK_NUM] = {
     ['}']               = { ASSOCIATE_RIGHT, END_PRECEDENCE },
     [']']               = { ASSOCIATE_RIGHT, END_PRECEDENCE },
 
-    // <expression> ::= <assignment-expression>
-    //                | <expression> , <assignment-expression>
     [',']               = { ASSOCIATE_LEFT, SEQUENCE_PRECEDENCE },
 
-    // <assignment-expression> ::= <conditional-expression>
-    //                           / <unary-expression> <assignment-operator> <assignment-expression>
     ['=']               = { ASSOCIATE_RIGHT, ASSIGN_PRECEDENCE, true },
     [TOK_ADD_ASSIGN]    = { ASSOCIATE_RIGHT, ASSIGN_PRECEDENCE, true },
     [TOK_AND_ASSIGN]    = { ASSOCIATE_RIGHT, ASSIGN_PRECEDENCE, true },
@@ -26,63 +22,33 @@ const AssocPrec g_assoc_prec[TOK_NUM] = {
     [TOK_SUB_ASSIGN]    = { ASSOCIATE_RIGHT, ASSIGN_PRECEDENCE, true },
     [TOK_XOR_ASSIGN]    = { ASSOCIATE_RIGHT, ASSIGN_PRECEDENCE, true },
 
-    // <conditional-expression> ::= <logical-or-expression>
-    //                            | <logical-or-expression> ? <expression> : <conditional-expression>
     ['?']               = { ASSOCIATE_RIGHT, CONDITIONAL_PRECEDENCE },
     [':']               = { ASSOCIATE_RIGHT, CONDITIONAL_PRECEDENCE },
 
-    // <logical-or-expression> ::= <logical-and-expression>
-    //                           | <logical-or-expression> || <logical-and-expression>
     [TOK_OR_OP]         = { ASSOCIATE_LEFT, LOGICAL_OR_PRECEDENCE },
 
-    // <logical-and-expression> ::= <inclusive-or-expression>
-    //                            | <logical-and-expression> && <inclusive-or-expression>
     [TOK_AND_OP]        = { ASSOCIATE_LEFT, LOGICAL_AND_PRECEDENCE },
 
-    // <inclusive-or-expression> ::= <exclusive-or-expression>
-    //                             | <inclusive-or-expression> | <exclusive-or-expression>
     ['|']               = { ASSOCIATE_LEFT, OR_PRECEDENCE },
 
-    // <exclusive-or-expression> ::= <and-expression>
-    //                             | <exclusive-or-expression> ^ <and-expression>
     ['^']               = { ASSOCIATE_LEFT, EXCLUSIVE_OR_PRECEDENCE },
 
-    // <and-expression> ::= <equality-expression>
-    //                    | <and-expression> & <equality-expression>
     ['&']               = { ASSOCIATE_LEFT, AND_PRECEDENCE },
 
-    // <equality-expression> ::= <relational-expression>
-    //                         | <equality-expression> == <relational-expression>
-    //                         | <equality-expression> != <relational-expression>
     [TOK_EQ_OP]         = { ASSOCIATE_LEFT, EQUALITY_PRECEDENCE },
     [TOK_NE_OP]         = { ASSOCIATE_LEFT, EQUALITY_PRECEDENCE },
 
-    // <relational-expression> ::= <shift-expression>
-    //                           | <relational-expression> < <shift-expression>
-    //                           | <relational-expression> > <shift-expression>
-    //                           | <relational-expression> <= <shift-expression>
-    //                           | <relational-expression> >= <shift-expression>
     ['<']               = { ASSOCIATE_LEFT, RELATIONAL_PRECEDENCE },
     ['>']               = { ASSOCIATE_LEFT, RELATIONAL_PRECEDENCE },
     [TOK_LE_OP]         = { ASSOCIATE_LEFT, RELATIONAL_PRECEDENCE },
     [TOK_GE_OP]         = { ASSOCIATE_LEFT, RELATIONAL_PRECEDENCE },
 
-    // <shift-expression> ::= <additive-expression>
-    //                      | <shift-expression> << <additive-expression>
-    //                      | <shift-expression> >> <additive-expression>
     [TOK_LEFT_OP]       = { ASSOCIATE_LEFT, SHIFT_PRECEDENCE },
     [TOK_RIGHT_OP]      = { ASSOCIATE_LEFT, SHIFT_PRECEDENCE },
 
-    // <additive-expression> ::= <multiplicative-expression>
-    //                         | <additive-expression> + <multiplicative-expression>
-    //                         | <additive-expression> - <multiplicative-expression>
     ['+']               = { ASSOCIATE_LEFT, ADDITIVE_PRECEDENCE },
     ['-']               = { ASSOCIATE_LEFT, ADDITIVE_PRECEDENCE },
 
-    // <multiplicative-expression> ::= <cast-expression>
-    //                               | <multiplicative-expression> * <cast-expression>
-    //                               | <multiplicative-expression> / <cast-expression>
-    //                               | <multiplicative-expression> % <cast-expression>
     ['*']               = { ASSOCIATE_LEFT, MULTIPLICATIVE_PRECEDENCE },
     ['/']               = { ASSOCIATE_LEFT, MULTIPLICATIVE_PRECEDENCE },
     ['%']               = { ASSOCIATE_LEFT, MULTIPLICATIVE_PRECEDENCE },
